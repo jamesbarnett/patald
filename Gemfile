b7fd1815d51053599987acdb722fc173cb8adf7a
Jim Barnett <james.barnett.jr@gmail.com>
2023-01-28 22:03:45 -0600

    Housekeeping stuff/removing windows cruft

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
-gem 'ruby-geometry', '~> 0.0.6'
-gem 'rspec', '~> 3.0.0'
+
 gem 'json', '~> 2.6.0'
 gem 'prawn', '~> 2.4.0'
-
+gem 'rspec', '~> 3.0.0'
+gem 'rubocop', '~> 1.44.1', require: false
+gem 'ruby-geometry', '~> 0.0.6'
--- lib/patald.rb
+++ lib/patald.rb
@@ -2,15 +2,23 @@
 
 require 'geometry'
 
+# base class for a quarter bodice component
 class SimpleBodice
   include Geometry
 
   attr_accessor :customer_id, :neck, :chest, :shoulders, :scye, :neck_to_waist, :shirt_length,
-                :center_back, :bicep, :wrist, :long_sleeve_length, :short_sleeve_length, 
+                :center_back, :bicep, :wrist, :long_sleeve_length, :short_sleeve_length,
                 :neck_offset, :origin, :seam_allowance, :points
-  
-  def initialize(neck, chest, neck_to_waist, shirt_length, scye, center_back,
-      torso_ease, neck_offset = 1.25, seam_allowance = 0.5)
+
+  def initialize(neck,
+                 chest,
+                 neck_to_waist,
+                 shirt_length,
+                 scye,
+                 center_back,
+                 torso_ease,
+                 neck_offset = 1.25,
+                 seam_allowance = 0.5)
     self.points = {}
     self.origin = Geometry::Point[0, 0]
     self.scye = scye
@@ -20,23 +28,24 @@ class SimpleBodice
     self.shirt_length = shirt_length
     self.center_back = center_back
     self.seam_allowance = seam_allowance
-    self.points[1] = Geometry::Point[self.origin.x, neck_offset]
-    self.points[2] = Geometry::Point[self.origin.x, self.scye + self.points[1].y]
-    self.points[3] = Geometry::Point[self.origin.x, self.neck_to_waist + self.points[1].y]
-    self.points[4] = Geometry::Point[self.origin.x, 
-      self.shirt_length + 2.0 * seam_allowance + self.points[1].y]
-    self.points[5] = Geometry::Point[self.origin.first, self.quarter_chest(torso_ease).y + self.points[1].y]
-    self.points[6] = Geometry::Point[self.points[5].x, self.points[5].y]
-    self.points[7] = Geometry::Point[self.points[5].x, self.points[3].y]
-    self.points[8] = Geometry::Point[self.points[5].x, self.points[4].y]
+    points[1] = Geometry::Point[origin.x, neck_offset]
+    points[2] = Geometry::Point[origin.x, scye + points[1].y]
+    points[3] = Geometry::Point[origin.x, neck_to_waist + points[1].y]
+    points[4] = Geometry::Point[origin.x,
+                                shirt_length + 2.0 * seam_allowance + points[1].y]
+    points[5] = Geometry::Point[origin.first,
+                                quarter_chest(torso_ease).y + points[1].y]
+    points[6] = Geometry::Point[points[5].x, points[5].y]
+    points[7] = Geometry::Point[points[5].x, points[3].y]
+    points[8] = Geometry::Point[points[5].x, points[4].y]
 
     # Above should possibly be extracted into "base grid" method
-    self.quarter_chest(torso_ease)
-    self.back_work
+    quarter_chest(torso_ease)
+    back_work
   end
 
-  def quarter_chest ease
-    Geometry::Point[0, self.chest / 4.0 + 2.0 * self.seam_allowance]
+  def quarter_chest(ease)
+    Geometry::Point[0, chest / 4.0 + ease * seam_allowance]
   end
 
   def back_work
