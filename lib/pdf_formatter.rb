5cdbf43216fd4d0dbf615f53572eba9b9cb3503f
Jim Barnett <james.barnett.jr@gmail.com>
2023-01-29 11:15:02 -0600

    Minor housekeeping

--- lib/pdf_formatter.rb
+++ lib/pdf_formatter.rb
@@ -19,12 +19,10 @@ class PdfFormatter
 
   def render(h)
     doc.stroke do
-      puts "Moving to first point"
-
       doc.move_to h.first.last.x, h.first.last.y
 
       h.each_key do |point|
-        puts "line_to: #{h[point].x} #{h[point].y}"
+        # Need to put some thinking into this for curves
         doc.line_to(h[point].x, h[point].y) if point != h.keys.first
       end
 
@@ -38,8 +36,6 @@ if __FILE__ == $0
   Prawn::Document.generate("hello.pdf") do |doc|
     doc.text 'Hello World!'
     formatter = PdfFormatter.new doc
-    # formatter.render({ 1 => Geometry::Point[10, 10], 
-    #                    2 => Geometry::Point[100, 100] })
     formatter.render(x.points)
   end
 
